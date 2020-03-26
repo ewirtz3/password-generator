6d12b6a574719a0687e11a435fee2cf07d21ab6e
Emily Wirtz <ewirtz3@gmail.com>
2020-03-26 12:52:34 -0500

    starting pseudo code

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+//prompt: password length 8-128 characters; if password length <8 or >128 characters, return 'Please enter a number between 8 and 128
+//prompt: include lowercase characters?
+//prompt: include uppercase characters?
+//prompt: include numbers?
+//prompt: include special characters?
+//
