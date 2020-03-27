7404b335286d220fda9e1ff0359347cc48ec5e83
Emily Wirtz <ewirtz3@gmail.com>
2020-03-26 22:08:13 -0500

    adjusted to .push method

--- script.js
+++ script.js
@@ -17,33 +17,30 @@ var passwordLength = prompt('Please specify a password length between 8 and 128 
   }
 }
 
-//var lowerCase = confirm: include lowercase characters?
+//define arrays, do I need new var names? lowerCase = [a-z]; upperCase = [A-Z], number = [0-9]; special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+']
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var num = '0123456789';
 var specialChar = '!@#$%^&*()+';
-var lower = confirm('Should the password include lower case letters?')
+//var lowerCase = confirm: include lowercase characters?
+var lower = confirm('Should the password include lower case letters?');
+//var upperCase = confirm: include uppercase characters?
 var upperCase = confirm('Should the password include upper case letters?');
+//var number = confirm: include numbers?
 var number = confirm('Should the password include numbers?');
-var special = confirm('Should the password include special characters?')
+//var special = confirm: include special characters?
+var special = confirm('Should the password include special characters?');
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword) {
   var characters = '';
-  (lowerCase.true) ? characters += lower;
-  (upperCase.true) ? characters += upper : '';
-  (number.true) ? characters += num : '';
-  (special.true) ? characters += specialChar : '';
+  (lowerCase.true) ? characters.push(...lower) : '';
+  (upperCase.true) ? characters.push(...upper) : '';
+  (number.true) ? characters.push(...num) : '';
+  (special.true) ? characters.push(...specialChar) : '';
   passwordText.value = password(pwdLength.value, characters);
 }
 
-
-
- 
-//var upperCase = confirm: include uppercase characters?
-//var number = confirm: include numbers?
-//var special = confirm: include special characters?
-//define arrays, do I need new var names? lowerCase = [a-z]; upperCase = [A-Z], number = [0-9]; special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+']
 //if lowerCase = true && upperCase = false && number = false && special = false {return [a-z]*passwordLength;} 
 //else if lowerCase = true && upperCase = true && number = false && special = false {var lowerUpper = [a-z].concat([A-Z]); return lowerUpper * passwordLength;}
 //else if lowerCase = true && upperCase = true && number = true && special = false {var lowerUpperNumber = [a-z].concat([A-Z], [0-9]); return lowerUpperNumber * passwordLength;}
