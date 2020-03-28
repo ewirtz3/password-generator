55112da1eb7cb57cc220da7bb42d94247921a9fe
Emily Wirtz <ewirtz3@gmail.com>
2020-03-28 14:35:29 -0500

    created for loop for password generation

--- script.js
+++ script.js
@@ -10,36 +10,43 @@ function writePassword() {
 
 }
 //var passwordLength = prompt: password length 8-128 characters; if password length <8 or >128 characters, alert 'Please enter a number between 8 and 128'
-var passwordLength = prompt('Please specify a password length between 8 and 128 characters.') {
-  var pwdLength = parseInt(passwordLength)
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert='Please enter a number between 8 and 128.'
-  }
+var passwordLength = prompt('Please specify a password length between 8 and 128 characters.');
+
+var pwdLength = parseInt(passwordLength)
+
+while (pwdLength <= 8 || pwdLength >= 128) {
+  alert('Please enter a number between 8 and 128.');
+  passwordLength = prompt ('Please specify a password length between 8 and 128 characters.');
+  pwdLength = parseInt(passwordLength);
 }
+
 
 //define arrays, do I need new var names? lowerCase = [a-z]; upperCase = [A-Z], number = [0-9]; special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+']
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var num = '0123456789';
 var specialChar = '!@#$%^&*()+';
 //var lowerCase = confirm: include lowercase characters?
 var lower = confirm('Should the password include lower case letters?');
 //var upperCase = confirm: include uppercase characters?
-var upperCase = confirm('Should the password include upper case letters?');
+var upper = confirm('Should the password include upper case letters?');
 //var number = confirm: include numbers?
 var number = confirm('Should the password include numbers?');
 //var special = confirm: include special characters?
 var special = confirm('Should the password include special characters?');
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword) {
-  var characters = '';
-  (lowerCase.true) ? characters.push(...lower) : '';
-  (upperCase.true) ? characters.push(...upper) : '';
-  (number.true) ? characters.push(...num) : '';
-  (special.true) ? characters.push(...specialChar) : '';
-}
+generateBtn.addEventListener("click", writePassword) 
 
+var characters = [];
+lower ? characters.push(...lowerCase) : '';
+upper ? characters.push(...upperCase) : '';
+number ? characters.push(...num) : '';
+special ? characters.push(...specialChar) : '';
+
+function generatePassword () {
+  for (i=0, i<characters.length, )
+}
 passwordText.value = generatePassword(pwdLength.value, characters);
 
 //if lowerCase = true && upperCase = false && number = false && special = false {return [a-z]*passwordLength;} 
